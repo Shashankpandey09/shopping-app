b162401f318b062d29d0a6eae1c4ff5ae25039ad
Shashankpandey09 <137607589+Shashankpandey09@users.noreply.github.com>
2024-03-02 17:18:34 -0800

    updated carousel

--- src/pages/CategoryProduct/CategoryProduct.jsx
+++ src/pages/CategoryProduct/CategoryProduct.jsx
@@ -5,19 +5,16 @@ import { AddItem, removeItem } from "../../Slices/Cart/CartSlice";
 import { toast, ToastContainer, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Caro from "../../Components/caros/Caro";
-
+import LazyLoad from 'react-lazy-load';
 
 const CategoryProduct = () => {
   const dispatch = useDispatch();
   const { cartItem: { products } } = useSelector((store) => store.HomeProduct);
   const { itemId } = useSelector((store) => store.cart);
-  
 
   useEffect(() => {
     dispatch(homeProducts());
   }, [dispatch]);
-
-
 
   const handleAddToCart = (item) => {
     dispatch(AddItem(item));
@@ -54,42 +51,50 @@ const CategoryProduct = () => {
   };
 
   return (
-    <div >
-    <Caro/>
-      <div className="bg-black w-[40%] text-center md:w-[20%] rounded-lg ml-4 p-4 mt-4">
+    <div className="bg-black text-white">
+      <Caro />
+
+      <div className="bg-gray-900 w-[40%] text-center md:w-[20%] rounded-lg ml-4 p-4 mt-4">
         <p className="text-white text-xl font-bold">See Our Products</p>
       </div>
-
+     
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-6">
         {products && products.map((item) => (
           <div
             key={item.id}
-            className="max-w-md mx-auto flex flex-col gap-4 bg-transparent text-gray-800 rounded-md overflow-hidden shadow-sm border mb-6 transform transition-transform duration-300 hover:scale-105"
-          >
+            className="max-w-md mx-auto w-[90vw]   md:w-fit flex flex-col gap-2  bg-white  text-gray-800 rounded-md overflow-hidden shadow-md border mb-6 transform transition-transform duration-300 hover:scale-105"
+          > 
+            <div className="relative w-full h-full ">
+            <LazyLoad height={150} width="auto"  threshold={0.95}>
             <img
-              className="w-full h-36 object-cover"
-              src={item.images[0]}
-              alt={item.title}
-            />
+                 className="w-full h-full object-cover rounded-t-md"
+                  src={item.images[0]}
+                  alt={item.title}
+                                  />
+           </LazyLoad>
+     
+            
+            </div>
+            
             <div className="p-4">
               <h2 className="text-xl font-bold text-gray-800">{item.title}</h2>
-              <p className="text-sm text-gray-600">{item.description}</p>
+              <p className="text-sm text-gray-500">{item.description}</p>
               <p className="mt-2 text-lg font-semibold text-gray-800">Price: ${item.price}</p>
-              <p className="text-gray-800">Rating: {item.rating}</p>
-              <p className="text-gray-800">Stock: {item.stock}</p>
-              <p className="text-green-600">Discount: {item.discountPercentage}% off</p>
+              <p className="text-gray-500">Rating: {item.rating}</p>
+              <p className="text-gray-500">Stock: {item.stock}</p>
+              <p className="text-green-500">Discount: {item.discountPercentage}% off</p>
             </div>
-            <div className="flex justify-end absolute bottom-2 right-1">
+            <div className="flex justify-end items-center  rounded-b-md p-4">
               {itemId.includes(item.id) ? (
                 <button
-                  className="mt-4 bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 focus:outline-none"
+                  className="bg-red-500 text-white px-4 py-3 rounded-full hover:bg-red-600 focus:outline-none"
                   onClick={() => handleRemoveFromCart(item)}
                 >
                   Remove
                 </button>
               ) : (
                 <button
-                  className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-800 focus:outline-none"
+                  className="bg-blue-500 text-white px-4 py-3 rounded-full hover:bg-blue-800 focus:outline-none"
                   onClick={() => handleAddToCart(item)}
                 >
                   Add to Cart
@@ -105,4 +110,3 @@ const CategoryProduct = () => {
 };
 
 export default CategoryProduct;
-
