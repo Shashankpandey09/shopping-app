f96485acc2c4c230f6c12a6fb89cf679d88dd03c
Shashankpandey09 <137607589+Shashankpandey09@users.noreply.github.com>
2024-11-12 03:44:08 -0800

    updated single product page

--- src/pages/ProductSinglePage/ProductSingle.jsx
+++ src/pages/ProductSinglePage/ProductSingle.jsx
@@ -9,9 +9,9 @@ import 'react-loading-skeleton/dist/skeleton.css'
 
 const ProductSingle = () => {
   const { userId } = useParams();
-  const { product ,status} = useSelector((store) => store.page);
-  const { itemId } = useSelector((store) => store.cart); // Change itemId to items
-
+  const { product, status } = useSelector((store) => store.page);
+  const { itemId } = useSelector((store) => store.cart);
+  
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getDetails(userId));
@@ -26,16 +26,8 @@ const ProductSingle = () => {
     toast.success('Item added to cart', {
       position: 'top-right',
       autoClose: 1000,
-      style: {
-        marginTop: window.innerWidth <= 768 ? '0%' : '0%',
-      },
+      style: { marginTop: window.innerWidth <= 768 ? '0%' : '0%' },
       hideProgressBar: false,
-      newestOnTop: false,
-      closeOnClick: true,
-      rtl: false,
-      pauseOnFocusLoss: true,
-      draggable: true,
-      pauseOnHover: true,
       theme: "dark",
       transition: Zoom,
     });
@@ -46,16 +38,7 @@ const ProductSingle = () => {
     toast.info('Item removed from cart', {
       position: 'top-right',
       autoClose: 1000,
-      hideProgressBar: false,
-      style: {
-        marginTop: window.innerWidth <= 768 ? '00%' : '0%',
-      },
-      newestOnTop: false,
-      closeOnClick: true,
-      rtl: false,
-      pauseOnFocusLoss: true,
-      draggable: true,
-      pauseOnHover: true,
+      style: { marginTop: window.innerWidth <= 768 ? '00%' : '0%' },
       theme: "dark",
       transition: Zoom,
     });
@@ -68,48 +51,42 @@ const ProductSingle = () => {
       setLightboxVisible(true);
     }
   };
-  
 
   const handleCloseLightbox = () => {
     setLightboxVisible(false);
   };
 
   if (!product || !product.images) {
-    return null; // or some loading/error component
+    return null;
   }
 
   if (status === 'loading') {
     return (
-      <div className="flex items-center justify-center  min-h-screen">
+      <div className="flex items-center justify-center min-h-screen">
         <div className="loader ease-linear border-4 border-t-4 border-t-red-500 h-24 w-24 animate-spin rounded-full"></div>
       </div>
     );
   }
-  
-
 
   return (
-    <div className="container mx-auto p-4 md:p-8 min-h-screen text-black   font-thin font-caveat">
+    <div className="container mx-auto p-4 md:p-8 min-h-screen text-black font-thin font-caveat">
       <div className="flex flex-col md:flex-row">
         <div className="md:w-1/2 relative h-[60vh]">
-          <div
-            className="absolute top-0 left-0 w-full h-full bg-opacity-75 rounded-lg"
-            onClick={() => handleImageClick(selectedImage, image)}
-          ></div>
-      <div className="w-full h-full relative">
-  {image || product.thumbnail ? (
-    <img
-      src={image || product.thumbnail}
-      alt={product.title}
-      loading="lazy"
-      className="w-full h-full object-cover absolute top-0 -z-20 rounded-lg shadow-lg cursor-pointer"
-    />
-  ) : (
-    <Skeleton baseColor="#202020" highlightColor="#444" />
-  )}
-</div>
-
+          <div className="absolute top-0 left-0 w-full h-full bg-opacity-75 rounded-lg" onClick={() => handleImageClick(selectedImage, image)}></div>
+          <div className="w-full h-full relative">
+            {image || product.thumbnail ? (
+              <img
+                src={image || product.thumbnail}
+                alt={product.title}
+                loading="lazy"
+                className="w-full h-full object-cover absolute top-0 -z-20 rounded-lg shadow-lg cursor-pointer"
+              />
+            ) : (
+              <Skeleton baseColor="#202020" highlightColor="#444" />
+            )}
+          </div>
         </div>
+
         <div className="md:w-1/2 md:pl-8">
           <h2 className="text-2xl md:text-4xl mb-2">{product.title}</h2>
           <p className="text-lg md:text-xl mb-4">{product.description}</p>
@@ -147,31 +124,27 @@ const ProductSingle = () => {
             key={index}
             src={img}
             alt={`Product ${index + 1}`}
-            className={`w-12 h-12 md:w-16 md:h-16 object-cover cursor-pointer rounded-lg ${
-              selectedImage === index ? 'border-4 border-blue-500' : 'border-2 border-gray-300'
-            } mr-2 md:mr-4`}
-            onClick={() => {setImage(img)
-            setSelectedImage(index)}}
+            className={`w-12 h-12 md:w-16 md:h-16 object-cover cursor-pointer rounded-lg ${selectedImage === index ? 'border-4 border-blue-500' : 'border-2 border-gray-300'} mr-2 md:mr-4`}
+            onClick={() => { setImage(img); setSelectedImage(index); }}
+            loading="lazy"
           />
         ))}
       </div>
+
       {lightboxVisible && (
-  <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 flex justify-center items-center z-50">
-    <div className="relative max-w-4xl max-h-4xl mx-auto">
-      <button
-        className="absolute top-4 right-4 text-red-500 text-4xl cursor-pointer focus:outline-none"
-        onClick={handleCloseLightbox}
-      >
-        &times;
-      </button>
-      <img
-        src={image}
-        alt="Lightbox"
-        className="w-full h-auto max-h-screen rounded-lg shadow-lg"
-      />
-    </div>
-  </div>
-)}
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 flex justify-center items-center z-50">
+          <div className="relative max-w-4xl max-h-4xl mx-auto">
+            <button className="absolute top-4 right-4 text-red-500 text-4xl cursor-pointer focus:outline-none" onClick={handleCloseLightbox}>
+              &times;
+            </button>
+            <img
+              src={image}
+              alt="Lightbox"
+              className="w-full h-auto max-h-screen rounded-lg shadow-lg"
+            />
+          </div>
+        </div>
+      )}
 
       <ToastContainer />
     </div>
