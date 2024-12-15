ef35976201be7e2bd5a8b591ac28f64391cf1191
Shashankpandey09 <137607589+Shashankpandey09@users.noreply.github.com>
2024-12-15 14:18:39 -0800

    updated and corrected firebase api

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,15 +8,18 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const ApiKEY=import.meta.env.VITE_FIREBASE_API_KEY
+console.log("Firebase API Key:", ApiKEY);
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: `${ApiKEY}`,
   authDomain: "shoppy-9032c.firebaseapp.com",
   projectId: "shoppy-9032c",
-  storageBucket: "shoppy-9032c.appspot.com",
+  storageBucket: "shoppy-9032c.firebasestorage.app",
   messagingSenderId: "610380318171",
   appId: "1:610380318171:web:79551eb5847a5a32b753fe",
   measurementId: "G-RYJ3HBWPG5"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
